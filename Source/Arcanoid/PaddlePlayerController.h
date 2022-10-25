0dc626e211728b450614edde3f86d105380f0d3a
Alexander Morodias <99577269+alexanderMorodias07@users.noreply.github.com>
2022-10-25 18:55:46 -0400

    Agregar archivos de proyecto.

---
+++ Source/Arcanoid/PaddlePlayerController.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PaddlePlayerController.generated.h"
+
+/**
+ * 
+ */
+class ABall;
+UCLASS()
+class ARCANOID_API APaddlePlayerController : public APlayerController
+{
+
+public:
+
+	APaddlePlayerController();
+private:
+	GENERATED_BODY()
+		UFUNCTION()
+
+		virtual void SetupInputComponent() override;
+
+
+
+protected:
+	virtual void BeginPlay() override;
+	void MoveHorizontal(float AxisValue);
+	void Lanch();
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<ABall> BallObj;
+
+	ABall* MyBall;
+	FVector SpawnLocation = FVector(10.0f, 0.0f, 40.0f);
+	FRotator SpawnRotation = FRotator(0.0f, 0.0f, 0.0f);
+	FActorSpawnParameters SpawnInfo;
+public:
+	void SpawnNewBall();
+
+};
